6b403274dc11eabc8be391858469882c4e33f7df
Phil <pbrede21@gmail.com>
2022-08-19 13:34:50 -0600

    removed hard coded credentials. And, deleted the api key since this'll be public

--- force-app/main/default/aura/ConversationContainer/ConversationContainerController.js
+++ force-app/main/default/aura/ConversationContainer/ConversationContainerController.js
@@ -1,26 +1,3 @@
 ({
-  init: function (cmp, ev, helper) {
-    const getConversations = cmp.get("c.getConversations");
-    getConversations.setParams({
-      apiKey: "SKd2c37d4e70773196ba48e6ab26f2c38d",
-      apiSecret: "ST9BlmoP9xcJyEVB0dhanXv2SN0gsHly",
-      phoneNumber: "%2B18475070348"
-    });
-
-    getConversations.setCallback(this, function (response) {
-      const state = response.getState();
-      console.log(state);
-      if (state !== "SUCCESS") throw Error("Failed to fetch conversations");
-      const result = JSON.parse(response.getReturnValue());
-      console.log(typeof result, result);
-      cmp.set(
-        "v.conversationSids",
-        result.conversations.map((convo) => convo.conversation_sid)
-      );
-
-      component.set("v.buttonLabel", myLabel);
-    });
-
-    $A.enqueueAction(getConversations);
-  }
+  init: function (cmp, ev, helper) {}
 });
