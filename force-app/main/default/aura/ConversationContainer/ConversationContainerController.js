1ac67b83150af054a4120cd4d665ba6702f3ded6
Phil <pbrede21@gmail.com>
2022-08-19 17:27:08 -0600

    updated

--- force-app/main/default/aura/ConversationContainer/ConversationContainerController.js
+++ force-app/main/default/aura/ConversationContainer/ConversationContainerController.js
@@ -2,53 +2,53 @@
   init: function (cmp, ev, helper) {
     console.log("ConversationContainer init");
 
-    const conversationSid = cmp.get("v.conversationSid");
-    console.log("conversationSid", conversationSid);
+    // const conversationSid = cmp.get("v.conversationSid");
+    // console.log("conversationSid", conversationSid);
 
-    const credential = cmp.get("v.credential");
-    console.log("credential", credential);
+    // const credential = cmp.get("v.credential");
+    // console.log("credential", credential);
 
-    const getMessages = cmp.get("c.getMessages");
-    const getParticipants = cmp.get("c.getParticipants");
+    // const getMessages = cmp.get("c.getMessages");
+    // const getParticipants = cmp.get("c.getParticipants");
 
-    const params = {
-      apiKey: credential.API_Key__c,
-      apiSecret: credential.Secret__c,
-      conversationSid
-    };
+    // const params = {
+    //   apiKey: credential.API_Key__c,
+    //   apiSecret: credential.Secret__c,
+    //   conversationSid
+    // };
 
-    getMessages.setParams(params);
-    getParticipants.setParams(params);
+    // getMessages.setParams(params);
+    // getParticipants.setParams(params);
 
-    getMessages.setCallback(this, function (res) {
-      const state = res.getState();
-      if (state !== "SUCCESS") throw Error("Failed to fetch conversations");
+    // getMessages.setCallback(this, function (res) {
+    //   const state = res.getState();
+    //   if (state !== "SUCCESS") throw Error("Failed to fetch conversations");
 
-      const result = JSON.parse(res.getReturnValue());
-      if (result.code) return console.error(result);
-      cmp.set("v.messages", result.messages);
+    //   const result = JSON.parse(res.getReturnValue());
+    //   if (result.code) return console.error(result);
+    //   cmp.set("v.messages", result.messages);
 
-      console.log("v.messages", "cmp.get", cmp.get("v.messages"));
-    });
+    //   console.log("v.messages", "cmp.get", cmp.get("v.messages"));
+    // });
 
-    getParticipants.setCallback(this, function (res) {
-      const state = res.getState();
-      if (state !== "SUCCESS") throw Error("Failed to fetch conversations");
+    // getParticipants.setCallback(this, function (res) {
+    //   const state = res.getState();
+    //   if (state !== "SUCCESS") throw Error("Failed to fetch conversations");
 
-      const result = JSON.parse(res.getReturnValue());
-      if (result.code) return console.error(result);
-      cmp.set("v.participants", result.participants);
+    //   const result = JSON.parse(res.getReturnValue());
+    //   if (result.code) return console.error(result);
+    //   cmp.set("v.participants", result.participants);
 
-      console.log("v.participants", "cmp.get", cmp.get("v.participants"));
+    //   console.log("v.participants", "cmp.get", cmp.get("v.participants"));
 
-      const participant = result.participants.find(
-        (person) => !JSON.parse(person.attributes).customer_id
-      );
-      cmp.set("v.participant", participant);
-      console.log("v.participant", "cmp.get", cmp.get("v.participant"));
-    });
+    //   const participant = result.participants.find(
+    //     (person) => !JSON.parse(person.attributes).customer_id
+    //   );
+    //   cmp.set("v.participant", participant);
+    //   console.log("v.participant", "cmp.get", cmp.get("v.participant"));
+    // });
 
-    $A.enqueueAction(getMessages);
-    $A.enqueueAction(getParticipants);
+    // $A.enqueueAction(getMessages);
+    // $A.enqueueAction(getParticipants);
   }
 });
