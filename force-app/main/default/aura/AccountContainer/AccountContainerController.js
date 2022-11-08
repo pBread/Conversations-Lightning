b343c931600a21da617597878ae3d3004f8e89ff
Phil <pbrede21@gmail.com>
2022-11-08 13:07:01 -0700

    working through account container

--- force-app/main/default/aura/AccountContainer/AccountContainerController.js
+++ force-app/main/default/aura/AccountContainer/AccountContainerController.js
@@ -1,9 +1,17 @@
 ({
   initialize: function (cmp, event, helper) {
+    console.log("initialize AccountContainer");
     const twilioAccount = cmp.get("v.twilioAccount");
 
     helper.getConversations(cmp, twilioAccount.Id).then(function (convos) {
       console.log("convos", convos);
+    });
+  },
+
+  tester: function (cmp, event, helper) {
+    ["contact", "twilioAccount"].forEach((attr) => {
+      const data = JSON.parse(JSON.stringify(cmp.get(`v.${attr}`)));
+      console.log(attr, data);
     });
   }
 });
