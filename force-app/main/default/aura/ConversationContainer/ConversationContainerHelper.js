bb64fd336ff7dcd3e9592ad4b4ef612ef4309435
Phil <pbrede21@gmail.com>
2022-12-01 12:52:21 -0700

    updated inbound message

--- force-app/main/default/aura/ConversationContainer/ConversationContainerHelper.js
+++ force-app/main/default/aura/ConversationContainer/ConversationContainerHelper.js
@@ -32,10 +32,19 @@
         const result = JSON.parse(res.getReturnValue());
         if (result.code) return reject(result);
 
-        const messages = result.messages.map((msg) =>
+        const messages = result.messages.map((msg, idx, arr) =>
           Object.assign(msg, {
             dateTime: new Date(msg.date_updated).toLocaleString(),
-            isInbound: msg.author.includes("@")
+            isInbound: msg.author.includes("@"),
+
+            showAuthor:
+              // show author only if this is last message
+              idx + 1 === arr.length ||
+              // show author only if the next message is not this author
+              arr[idx + 1].author !== msg.author ||
+              // show author only if next message isn't until next day
+              new Date(arr[idx + 1].date_updated).toDateString() !==
+                new Date(msg.date_updated).toDateString()
           })
         );
 
