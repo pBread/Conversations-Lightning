2f0240ff470e86088f059ef05c6870a7c008d60e
Phil <pbrede21@gmail.com>
2022-12-01 17:04:16 -0700

    added selectedIdentity

--- force-app/main/default/aura/AccountContainer/AccountContainerController.js
+++ force-app/main/default/aura/AccountContainer/AccountContainerController.js
@@ -7,22 +7,44 @@
     });
   },
 
+  handleIdentitySelection: function (cmp, event, helper) {
+    const identities = JSON.parse(JSON.stringify(cmp.get("v.identities")));
+
+    const identitySelection = cmp.find("identitySelection");
+    const value = identitySelection.get("v.value");
+
+    cmp.set(
+      "v.selectedIdentity",
+      identities.find((identity) => identity.name === value).email
+    );
+  },
+
   handleUpdateIdentities: function (cmp, event, helper) {
     const email = event.getParam("email");
     const name = event.getParam("name");
+    const selectedIdentity = event.getParam("selectedIdentity");
 
     const identities = JSON.parse(JSON.stringify(cmp.get("v.identities")));
-
-    console.log("handleUpdateIdentities", { email, name, identities });
     cmp.set("v.identities", identities.concat({ email, name }));
+
+    if (!selectedIdentity) {
+      cmp.set("v.selectedIdentity", email);
+
+      const identitySelection = cmp.find("identitySelection");
+      identitySelection.set("v.value", name);
+    }
   },
 
   tester: function (cmp, event, helper) {
-    ["contact", "conversations", "identities", "twilioAccount"].forEach(
-      (attr) => {
-        const data = JSON.parse(JSON.stringify(cmp.get(`v.${attr}`)));
-        console.log(attr, data);
-      }
-    );
+    [
+      "contact",
+      "conversations",
+      "identities",
+      "selectedIdentity",
+      "twilioAccount"
+    ].forEach((attr) => {
+      const data = JSON.parse(JSON.stringify(cmp.get(`v.${attr}`)));
+      console.log(attr, data);
+    });
   }
 });
